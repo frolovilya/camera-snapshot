5a3053faccc353e477868e9403bd85ded1acd554
Ilya Frolov <fiss205@gmail.com>
2019-10-12 13:57:01 +0300

    Fixed tests.

--- src/test/test_config.py
+++ src/test/test_config.py
@@ -11,13 +11,13 @@ class TestConfig(unittest.TestCase):
     _secret_placeholder = "(secret)"
 
     def setUp(self):
-        os.environ['TIME_PERIOD'] = str(100)
+        os.environ['TIME_PERIOD_SEC'] = str(100)
         os.environ['FFMPEG_BIN'] = "/usr/local/bin/ffmpeg"
 
     def test_properties_override(self):
         props = config.get_properties(self._config_file_path)
 
-        self.assertEqual(props['time_period'], os.environ['TIME_PERIOD'])
+        self.assertEqual(props['time_period_sec'], os.environ['TIME_PERIOD_SEC'])
         self.assertEqual(props['ffmpeg']['bin'], os.environ['FFMPEG_BIN'])
 
         self.assertEqual(props['s3']['bucket_name'], self._secret_placeholder)
