4044f745847d409598cdcc1123d4855c203777ec
Ilya Frolov <fiss205@gmail.com>
2019-11-03 16:46:06 +0300

    Added support for remote cameras configuration via M3U8 playlist file.

--- src/config.py
+++ src/config.py
@@ -1,7 +1,9 @@
 import yaml
 import os
+import m3u8
 
 import logger
+import webcam
 
 
 def _override_with_env_variables(props: dict, prefix=""):
@@ -31,6 +33,25 @@ def _override_with_env_variables(props: dict, prefix=""):
                 props[key] = override
 
 
+def _read_m3u8_cameras_config(playlist_uri: str) -> list:
+    """
+    Read cameras configuration from remote m3u8 file.
+
+    :param playlist_uri: m3u8 file location
+    :return: list of Cameras
+    """
+    logger.log("Reading cameras configuration from m3u8 playlist {}", playlist_uri)
+    playlist = m3u8.load(playlist_uri)
+    return [webcam.Camera(s.value, p.uri)
+            for p, s in zip(playlist.playlists, playlist.session_data)
+            if s.data_id == "camera.name"]
+
+
+def _init_cameras(props: dict):
+    if props["cameras"] is None or len(props["cameras"]) == 0:
+        props["cameras"] = _read_m3u8_cameras_config(props['cameras_playlist'])
+
+
 def get_properties(yaml_file: str) -> dict:
     """
     Read properties from Yaml file.
@@ -41,4 +62,5 @@ def get_properties(yaml_file: str) -> dict:
     with open(yaml_file) as yaml_file_handler:
         config = yaml.load(yaml_file_handler, yaml.Loader)
     _override_with_env_variables(config)
+    _init_cameras(config)
     return config
